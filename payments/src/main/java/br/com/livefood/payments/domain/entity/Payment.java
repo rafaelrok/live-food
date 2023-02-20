2ba3f5dbf10a62147b172021c0436abc8b685779
rafaelrok <rafaelvieirasantos2006@hotmail.com>
2023-02-20 17:00:26 -0300

    Crated entity payment with validation

---
+++ payments/src/main/java/br/com/livefood/payments/domain/entity/Payment.java
@@ -0,0 +1,59 @@
+package br.com.livefood.payments.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "payments")
+public class Payment implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Positive
+    private BigDecimal amount;
+
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+
+    @NotBlank
+    @Size(max = 19)
+    private String number;
+
+    @NotBlank
+    @Size(max = 7)
+    private String expiration;
+
+    @NotBlank
+    @Size(min = 3, max = 3)
+    private String code;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @NotNull
+    private Long orderId;
+
+    @NotNull
+    private Long paymentMathodId;
+
+
+}
---
+++ payments/src/main/java/br/com/livefood/payments/domain/entity/Status.java
@@ -0,0 +1,9 @@
+package br.com.livefood.payments.domain.entity;
+
+/**
+ * @author rafae
+ */
+
+public enum Status {
+    PENDING, APPROVED, CANCELED
+}
